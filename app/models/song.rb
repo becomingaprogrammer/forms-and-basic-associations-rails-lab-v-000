621a04568acec163c83f2b42dc5efdae0ec13802
Dakota L Martinez <dakotaleemusic@gmail.com>
2020-10-08 16:08:17 -0700

    finish song model specs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,21 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  # validations
+  # validates :title, :artist_id, :genre_id, presence: true
+  # validates :title, uniqueness: { scope: [:artist_id]}
+
+  validates :artist_id, :genre_id, {presence: true}
+  validates :title, {presence: true, uniqueness: { scope: [:artist_id]}}
+
+  def artist_name=(artist_name)
+    self.artist = Artist.find_or_create_by(name: artist_name) 
+  end
+
+  def artist_name
+    self.artist.try(:name)
+  end
 end
