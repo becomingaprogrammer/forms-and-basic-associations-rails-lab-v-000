6983eadb9c954f314c05a1fd2803c878294bf4c2
Dakota L Martinez <dakotaleemusic@gmail.com>
2020-10-08 18:01:11 -0700

    finish new song form

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,7 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
-
+    byebug if ENV['RAILS_ENV'] == "development"
     if @song.save
       redirect_to @song
     else
@@ -45,7 +45,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, note_contents: [])
   end
 end
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -8,8 +8,8 @@ class Song < ActiveRecord::Base
   # validates :title, :artist_id, :genre_id, presence: true
   # validates :title, uniqueness: { scope: [:artist_id]}
 
-  validates :artist_id, :genre_id, {presence: true}
-  validates :title, {presence: true, uniqueness: { scope: [:artist_id]}}
+  # validates :artist_id, :genre_id, {presence: true}
+  # validates :title, {presence: true, uniqueness: { scope: [:artist_id]}}
 
   def artist_name=(artist_name)
     self.artist = Artist.find_or_create_by(name: artist_name) 
@@ -17,5 +17,27 @@ class Song < ActiveRecord::Base
 
   def artist_name
     self.artist.try(:name)
+  end
+
+  def note_1
+    self.notes.first.try(:content)
+  end
+
+  def note_1=(content)
+    @note_1 = self.notes.build(content: content)
+  end
+
+  def note_2
+    self.notes.last.try(:content)
+  end
+
+  def note_2=(content)
+    @note_2 = self.notes.build(content: content)
+  end
+
+  def note_contents=(notes_content)
+    notes_content.each do |note_content|
+      self.notes.build(content: note_content)
+    end
   end
 end
